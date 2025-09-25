324256ffe718f3f914b16471c5b9872f3948c880
Hankin <554553400@qq.com>
2025-09-25 12:03:39 +0800

    UPDATE ScriptDir

--- main/sql/tables_init.sql
+++ main/sql/tables_init.sql
@@ -14,3 +14,18 @@ CREATE TABLE IF NOT EXISTS qiniu_cdnauth_requests (
     );
 CREATE INDEX IF NOT EXISTS "IDX_client_ip" ON qiniu_cdnauth_requests USING btree (client_ip);
 CREATE INDEX IF NOT EXISTS "IDX_http_referer" ON qiniu_cdnauth_requests USING btree (http_referer);
+
+CREATE TABLE IF NOT EXISTS qiniu_cdnauth_block_requests (
+        id SERIAL PRIMARY KEY,
+		request_id int8,
+		client_ip VARCHAR(45),
+		x_forwarded_for VARCHAR(255),
+		user_agent VARCHAR(500),
+		http_referer VARCHAR(255),
+		request_url varchar(1000) NOT NULL,
+		request_headers json NOT NULL,
+		raw_url varchar(1000) NOT NULL,
+		deleted_at timestamp NULL,
+		created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+		updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+)
--- script.go
+++ script.go
@@ -3,11 +3,10 @@ package cdnguard
 import (
 	"embed"
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 	"sync"
-
-	"github.com/iotames/miniutils"
 )
 
 type ScriptDir struct {
@@ -18,13 +17,18 @@ type ScriptDir struct {
 var onesd *ScriptDir
 var once sync.Once
 
+// GetScriptDir 获取脚本目录单例
 func GetScriptDir(sd *ScriptDir) *ScriptDir {
 	once.Do(func() {
 		onesd = sd
 	})
+	if onesd == nil {
+		panic("ScriptDir is nil")
+	}
 	return onesd
 }
 
+// NewScriptDir 初始化一个程序运行时所需的外部脚本文件目录。
 func NewScriptDir(customDir, defaultDir string, embedFs embed.FS) *ScriptDir {
 	return &ScriptDir{DefaultDir: defaultDir, CustomDir: customDir, embedFS: embedFs}
 }
@@ -33,7 +37,7 @@ func NewScriptDir(customDir, defaultDir string, embedFs embed.FS) *ScriptDir {
 // replaceList 字符串列表，依次替换SQL文本中的?占位符
 // TODO 需要强调占位符与通配符的区别，比如%和_在LIKE子句中不是占位符，而是通配符，需要和参数化查询中的占位符区分开。
 func (s ScriptDir) GetSQL(fpath string, replaceList ...string) (string, error) {
-	sqlTxt, err := s.getSqlText(fpath)
+	sqlTxt, err := s.GetScriptText(fpath)
 	if err != nil {
 		return "", err
 	}
@@ -43,39 +47,66 @@ func (s ScriptDir) GetSQL(fpath string, replaceList ...string) (string, error) {
 	return sqlTxt, nil
 }
 
-// getSqlText 获取sql文本
-// 优先从custom/sql自定义目录读取sql。如找不到SQL文件，则从默认的sql目录中读取。如再找不到文件，则从内嵌文件中读取。
-func (s ScriptDir) getSqlText(fpath string) (sqlTxt string, err error) {
-	defaultFilePath := filepath.Join(s.DefaultDir, fpath)
-	customDirPath := filepath.Join(s.CustomDir, fpath)
-	// 优先从custom/sql自定义目录读取sql。如找不到SQL文件，则从默认的sql目录中读取。
-	sqlTxt, err = GetTextByFilePath(defaultFilePath, customDirPath)
-	if sqlTxt == "" {
-		// 找不到文件，从内嵌的文件中读取sql文件
-		var sqlBytes []byte
-		sqlBytes, err = s.embedFS.ReadFile(fpath)
-		if err != nil {
-			return
+func (s ScriptDir) OkDir(d string) error {
+	info, err := os.Stat(d)
+	if err == nil {
+		if !info.IsDir() {
+			return fmt.Errorf("%s is not a directory", d)
 		}
-		sqlTxt = string(sqlBytes)
+		return err
 	}
-	return
+	if os.IsNotExist(err) {
+		return fmt.Errorf("dir(%s) does not exist", d)
+	}
+	return err
 }
 
-func getExistFile(defaultFilePath string, customFilePath string) string {
-	if miniutils.IsPathExists(customFilePath) {
+func (s ScriptDir) OkNormalFile(d string) error {
+	info, err := os.Stat(d)
+	if err == nil {
+		if info.IsDir() {
+			return fmt.Errorf("%s is not a directory", d)
+		}
+		return err
+	}
+	if os.IsNotExist(err) {
+		return fmt.Errorf("file(%s) does not exist", d)
+	}
+	return err
+}
+
+// GetScriptText 获取脚本文件的纯文本内容
+// 优先从CustomDir自定义目录查找文件。如找不到，则从DefaultDir默认目录查找。如再找不到文件，则从内嵌文件中读取。
+func (s ScriptDir) GetScriptText(fpath string) (stxt string, err error) {
+	var b []byte
+	customDirPath := filepath.Join(s.CustomDir, fpath)
+	defaultFilePath := filepath.Join(s.DefaultDir, fpath)
+	realfpath := s.getExistFile(customDirPath, defaultFilePath)
+	if realfpath != "" {
+		// 找到目标文件
+		b, err = os.ReadFile(realfpath)
+		stxt = string(b)
+	}
+	if stxt != "" && err == nil {
+		// 文件内容不为空，读取没有错误
+		return stxt, err
+	}
+	// 找不到文件，获取读取了文件内容为空。从内嵌的文件中读取sql文件
+
+	b, err = s.embedFS.ReadFile(fpath)
+	if err != nil {
+		return stxt, err
+	}
+	stxt = string(b)
+	return stxt, err
+}
+
+func (s ScriptDir) getExistFile(customFilePath string, defaultFilePath string) string {
+	if s.OkNormalFile(customFilePath) == nil {
 		return customFilePath
 	}
-	if miniutils.IsPathExists(defaultFilePath) {
+	if s.OkNormalFile(defaultFilePath) == nil {
 		return defaultFilePath
 	}
 	return ""
-}
-
-func GetTextByFilePath(defaultFilePath string, customFilePath string) (content string, err error) {
-	fpath := getExistFile(defaultFilePath, customFilePath)
-	if fpath == "" {
-		return "", fmt.Errorf("file not found: %s", defaultFilePath)
-	}
-	return miniutils.ReadFileToString(fpath)
 }
